daafcf26fc8427501feba179380c0d44c55f7a03
Amir Razavi <s.amirhosein.rzv@gmail.com>
2025-05-15 20:55:43 +0330

    ye taiqr kochik

---
+++ 1_Limit/limit_main.py
@@ -0,0 +1,24 @@
+"""
+
+[1, 2, 3, 4, 5]
+we have min and max for a list and if we have
+min(3) it gives us this : [3, 4, 5]
+and
+max(3) it gives us this : [1, 2, 3]
+and if both at the same time
+min, max = 3 -> [3]
+
+"""
+
+# My way
+data = [1, 2, 3, 4, 5]
+
+min_ = int(input("Enter min : "))
+max_ = int(input("Enter max : "))
+if min_ != 0 and max_ != 0:
+    pass
+
+elif min_ != 0:
+    pass
+elif max_ != 0:
+    pass
