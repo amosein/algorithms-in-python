f626c1e8d0101d626e584ec72e1eb35dc32c7ba7
Amir Razavi <s.amirhosein.rzv@gmail.com>
2025-05-31 23:40:43 +0330

    added the best practice way for this

--- 01 - Limit/01 - main.py
+++ 01 - Limit/01 - main.py
@@ -11,6 +11,15 @@ min, max = 3 -> [3]
 
 """
 
+# Best Practice
+def limit(data, min=None, max=None):
+    min_check = lambda val: True if min is None else (val >= min)
+    max_check = lambda val: True if max is None else (val <= max)
+
+    return [val for val in data if min_check(val) and max_check(val)]
+
+print(limit([1, 2, 3, 4, 5], max=3))
+
 # # My way - 1
 # data = [1, 2, 3, 4, 5]
 #
@@ -24,25 +33,25 @@ min, max = 3 -> [3]
 # elif max_ != 0:
 #     pass
 
-# My way 2
-def limit(data: List, max_ :int = 0, min_ :int = 0):
-    result = []
-
-    if max_ == 0:
-        for i in data:
-            if i <= min_:
-                result.append(i)
-    elif min_ == 0:
-        for j in data:
-            if j >= max_:
-                result.append(j)
-
-    elif max_ == min_:
-        return [max_]
-
-    return result
-
-data = [1, 2, 3, 4, 5]
-# max_ = int(input("Max = "))
-# min_ = int(input("Min = "))
-print(limit(data, 3, 0))
+# # My way 2
+# def limit(data: List, max_ :int = 0, min_ :int = 0):
+#     result = []
+#
+#     if max_ == 0:
+#         for i in data:
+#             if i <= min_:
+#                 result.append(i)
+#     elif min_ == 0:
+#         for j in data:
+#             if j >= max_:
+#                 result.append(j)
+#
+#     elif max_ == min_:
+#         return [max_]
+#
+#     return result
+#
+# data = [1, 2, 3, 4, 5]
+# # max_ = int(input("Max = "))
+# # min_ = int(input("Min = "))
+# print(limit(data, 3, 0))
