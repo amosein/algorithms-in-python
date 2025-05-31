1b81d81eb83a31baa2e4deee3095150725069938
Amir Razavi <s.amirhosein.rzv@gmail.com>
2025-05-31 23:31:46 +0330

    second try and its done

--- 01 - Limit/01 - main.py
+++ 01 - Limit/01 - main.py
@@ -1,3 +1,4 @@
+from typing import List
 """
 
 [1, 2, 3, 4, 5]
@@ -10,15 +11,38 @@ min, max = 3 -> [3]
 
 """
 
-# My way
+# # My way - 1
+# data = [1, 2, 3, 4, 5]
+#
+# min_ = int(input("Enter min : "))
+# max_ = int(input("Enter max : "))
+# if min_ != 0 and max_ != 0:
+#     pass
+#
+# elif min_ != 0:
+#     pass
+# elif max_ != 0:
+#     pass
+
+# My way 2
+def limit(data: List, max_ :int = 0, min_ :int = 0):
+    result = []
+
+    if max_ == 0:
+        for i in data:
+            if i <= min_:
+                result.append(i)
+    elif min_ == 0:
+        for j in data:
+            if j >= max_:
+                result.append(j)
+
+    elif max_ == min_:
+        return [max_]
+
+    return result
+
 data = [1, 2, 3, 4, 5]
-
-min_ = int(input("Enter min : "))
-max_ = int(input("Enter max : "))
-if min_ != 0 and max_ != 0:
-    pass
-
-elif min_ != 0:
-    pass
-elif max_ != 0:
-    pass
+# max_ = int(input("Max = "))
+# min_ = int(input("Min = "))
+print(limit(data, 3, 0))
